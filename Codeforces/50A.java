b62286aa52225aa07b5882c799bb889c6241b2ba
boctorio <46447053+boctorio@users.noreply.github.com>
2020-06-22 14:29:15 +0800

    Create 50A.java

---
+++ Codeforces/50A.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.stream.*;
+import java.io.*;
+public class Test {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n, m;
+        n = sc.nextInt();
+        m = sc.nextInt();
+        System.out.print(n * m / 2);
+    }
+}
